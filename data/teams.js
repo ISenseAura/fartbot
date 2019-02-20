3c3cb68d757fcadfc2eac77d0be2dcc0b25cf470
fart <tyler.m.sciara@gmail.com>
2019-02-20 11:56:23 -0500

    Add files via upload

---
+++ data/teams.js
@@ -0,0 +1,11 @@
+'use strict';
+
+exports.BattlePokeTeams = [
+	["Red", "Blue"],
+	["Gold", "Silver"],
+	["Ruby", "Sapphire"],
+	["Diamond", "Pearl"],
+	["Black", "White"],
+	["X", "Y"],
+	["Sun", "Moon"],
+];
